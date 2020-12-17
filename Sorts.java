82ea74c77dc2ee6011b4f82d5d9df7a26f6252a5
xiaopies <xiaoshenma2016@gmail.com>
2020-12-17 01:12:29 -0500

    wrote out initial method two days ago and forgot to commit

---
+++ Sorts.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+public class Sorts{
+  /**Bubble sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static int miniSort(int[] data){
+    int x = 0;
+    for ( int i = 0; i < data.length-1; i++){
+      int a = 0;
+      if (data[i]>data[i+1]){
+        a = data[i+1];
+        data[i+1] = data[i];
+        data[i] = a;
+        x++;
+      }
+      System.out.println(data);
+    }
+    return x;
+  }
+  public static void bubbleSort(int[] data){
+
+    for (int i = 0; i<data.length; i++){
+      if (miniSort(data) == 0){
+        break;
+      }
+      else{
+        miniSort(data);
+      }
+    }
+  }
+}

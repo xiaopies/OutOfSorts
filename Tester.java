c5619e2d5f071269e2ec74a73bbf015ff50b2577
xiaopies <xiaoshenma2016@gmail.com>
2020-12-17 11:01:43 -0500

    tried a lot of numbers

--- Tester.java
+++ Tester.java
@@ -2,6 +2,12 @@ import java.io.*;
 import java.util.*;
 import java.util.Random;
 public class Tester{
+  public static void ran(int[] args){
+    Random rando = new Random(100);
+    for (int i =0; i<args.length; i++){
+      args[i] = rando.nextInt() % 10000;
+    }
+  }
   public static void main(String[] args){
     int[] a = {1, 2, 3, 4, 5};
     int[] b = {5, 4, 3, 2, 1};
@@ -9,6 +15,8 @@ public class Tester{
     int[] d = {-7, 100, 5, -9, 0, -1, };
     int[] e = {0, 0, 0, 0, 0};
     int[] f = {9, 10000, 42, 4};
+    int[] g = new int[10000];
+    ran(g);
 
     OutSorts.main(a);
 
@@ -21,7 +29,13 @@ public class Tester{
     OutSorts.main(e);
 
     OutSorts.main(f);
-    
+
+    OutSorts.main(g);
+    System.out.println("after");
+    System.out.println(Arrays.toString(g));
+
+
+
   }
 
 }
